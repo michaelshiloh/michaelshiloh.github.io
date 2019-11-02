b91d93fd01488de3d1a8901325e8cad6e06344ae
Michael Shiloh <michael@michaelshiloh.com>
2019-11-02 17:24:16 +0400

    adding text to the other projects

---
+++ media/projects/furniture/text.js
@@ -0,0 +1,1 @@
+document.write("A series of furniture primarily using steel and broken glass")
---
+++ media/projects/linuxRobot/text.js
@@ -0,0 +1,1 @@
+document.write("A robot to which anyone on the Internet can connect, and once connected, can program it to perform various behaviors")
